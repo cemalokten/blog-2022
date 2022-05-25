7b17a2100ebdeab7f1c931ac3bd87efcf729f42e
cemalokten <cemalokten@gmail.com>
2022-05-25 18:53:22 +0100

    Preact setup

---
+++ client/preact.config.js
@@ -0,0 +1,34 @@
+module.exports = function (config) {
+  if (config.devServer) {
+    config.devServer.proxy = [
+      {
+        // proxy requests matching a pattern:
+        path: "/api/**",
+
+        // where to proxy to:
+        target: "http://localhost:3000",
+
+        // optionally change Origin: and Host: headers to match target:
+        changeOrigin: true,
+        changeHost: true,
+        secure: false,
+
+        // optionally mutate request before proxying:
+        pathRewrite: function (path, request) {
+          // you can modify the outbound proxy request here:
+          // delete req.headers.referer;
+
+          // common: remove first path segment: (/api/**)
+          return "/" + path.replace(/^\/[^\/]+\//, "");
+        },
+
+        // optionally mutate proxy response:
+        // onProxyRes: function(proxyRes, req, res) {
+        // you can modify the response here:
+        // proxyRes.headers.connection = 'keep-alive';
+        // proxyRes.headers['cache-control'] = 'no-cache';
+        // }
+      },
+    ];
+  }
+};
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import helmet from "helmet";
 import router from "./routes";
+
+const morgan = require("morgan");
 
 const app = express();
 
 const port = 3000;
 const hostname = "localhost";
 
+app.use(helmet());
+app.use(morgan("tiny"));
 app.use("/api", router);
 
 app.listen(port, hostname, () => {
