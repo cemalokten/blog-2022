559c4c253022e5fd5b07f092a1f8ddc07ddc2055
cemalokten <cemalokten@gmail.com>
2022-09-30 20:02:10 +0100

    Fix fontSize globally

--- client/src/style/globalStyles.ts
+++ client/src/style/globalStyles.ts
@@ -3,35 +3,30 @@ import emotionReset from "emotion-reset";
 
 const global = css`
   ${emotionReset},
-  html, body, #root, #root>div {
+  html {
+    box-sizing: border-box;
+    overflow-x: hidden;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  html,
+  body,
+  #root,
+  #root > div {
     color: black;
     margin: 0vw;
     font-family: "Helvetica Neue";
-  }
-
-  h1 {
-    font-weight: 500;
-    margin-bottom: 20px;
+    font-size: 28px;
+    height: 100%;
   }
 
   pre {
     margin: 30px 0;
     font-size: 20px;
-  }
-  .fade-enter {
-    opacity: 0;
-    z-index: 1;
-  }
-  .fade-enter.fade-enter-active {
-    opacity: 1;
-    transition: opacity 0.5s ease-in-out;
-  }
-  .fade-exit {
-    opacity: 1;
-  }
-  .fade-exit.fade-exit-active {
-    opacity: 0;
-    transition: opacity 0.2s ease-in-out;
   }
 `;
 
--- client/src/theme/components/link.ts
+++ client/src/theme/components/link.ts
@@ -1,27 +1,25 @@
 import type { ComponentStyleConfig } from "@chakra-ui/theme";
-import { P } from "../../constants/theme";
 
 const Link: ComponentStyleConfig = {
   baseStyle: {
-    borderBottom: "0px solid transparent",
     fontFamily: "Helvetica Neue",
     fontStyle: "normal",
     fontWeight: "400",
     letterSpacing: "0.02em",
+    lineHeight: "45px",
+    margin: "0",
     pointerEvents: "auto",
     textAlign: "left",
     transition: "color 100ms ease-in-out",
-    fontSize: P,
 
     _hover: {
-      textDecoration: "none",
       color: "blue",
+      textDecoration: "none",
     },
 
     _active: {
       color: "blue",
       textDecoration: "none",
-      borderBottom: "0px solid transparent",
     },
   },
 };
