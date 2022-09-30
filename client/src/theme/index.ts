44708f920cf9c238e350ef7ee1b9b42ff1755a7d
cemalokten <cemalokten@gmail.com>
2022-09-30 20:01:24 +0100

    Add heading to theme

--- client/src/theme/index.ts
+++ client/src/theme/index.ts
@@ -1,11 +1,13 @@
 import { extendTheme } from "@chakra-ui/react";
 import { Link } from "./components/link";
 import { Text } from "./components/paragraph";
+import { Heading } from "./components/heading";
 
 const overrides = {
   components: {
     Link,
     Text,
+    Heading,
   },
 };
 
