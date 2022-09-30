6fe837d716f2f14e2eea845100701823ac328214
cemalokten <cemalokten@gmail.com>
2022-09-30 20:09:40 +0100

    Add heading to theme

---
+++ client/src/theme/components/heading.ts
@@ -0,0 +1,13 @@
+import type { ComponentStyleConfig } from "@chakra-ui/theme";
+
+const Heading: ComponentStyleConfig = {
+  baseStyle: {
+    letterSpacing: "0.02em",
+    lineHeight: "45px",
+    fontStyle: "normal",
+    fontWeight: "400",
+    marginBottom: "0",
+  },
+};
+
+export { Heading };
