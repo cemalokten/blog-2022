4710ac9f6bd0ae495b2f1df9bc5cc17c29a6a539
cemalokten <cemalokten@gmail.com>
2022-10-07 13:53:47 +0100

    Update tag style order

--- client/src/components/Tag/index.tsx
+++ client/src/components/Tag/index.tsx
@@ -3,26 +3,26 @@ import styled from "@emotion/styled";
 import { Tag as TagType } from "../../constants/Types";
 
 export const _Tag = styled.span<TagType>`
+  font-family: "Lab Mono";
+  font-weight: 400;
   position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
+  pointer-events: none;
+  transition: opacity 0.25s ease-in-out;
+  white-space: pre;
+  z-index: 1;
   transform: ${({ rotation }) => `rotate(${rotation}deg)`};
   left: ${({ left }) => left};
   top: ${({ top }) => top};
   background-color: ${({ backgroundColor }) => backgroundColor};
   opacity: ${({ opacity }) => opacity};
-  transition: opacity 0.25s ease-in-out;
   border-radius: ${({ radius = 0.3 }) => radius + "em"};
   border: ${({ border }) => (border ? "solid 0.2rem black" : "none")};
   font-size: ${({ fontSize }) => fontSize};
-  font-family: "Lab Mono";
-  font-weight: 400;
-  pointer-events: none;
   padding: ${({ padding }) => padding + "px"};
   height: ${({ height }) => height + "em"};
-  white-space: pre;
-  z-index: 1;
 `;
 
 const Tag: FC<TagType> = ({ label, color, ...props }) => {
