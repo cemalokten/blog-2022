810f7aa8d1e1921f94d5dcaa2835aeb311860edb
cemalokten <cemalokten@gmail.com>
2022-10-07 13:53:28 +0100

    Update layout

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import * as C from "../../components";
-import { Flex, Box, Link } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { Flex, Box, Link } from "@chakra-ui/react";
 
 const Layout = () => {
   return (
-    <Flex flexDirection="column" p={10}>
+    <Flex flexDirection="column" p={6}>
       <Box id="canvas" flexGrow={1} pb={10}>
         <C.Header />
         <Outlet />
